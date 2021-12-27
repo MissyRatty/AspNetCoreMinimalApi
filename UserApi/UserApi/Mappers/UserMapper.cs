2f82d344f37eb3a418b1ddbd219aec2080b19386
rahmat <rahmat.boakye-agyemang@microlise.com>
2021-12-27 21:58:30 +0000

    Adding Mappers

---
+++ UserApi/UserApi/Mappers/UserMapper.cs
@@ -0,0 +1,18 @@
+﻿using UserApi.Dtos;
+using UserApi.Models;
+
+namespace UserApi.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserModel ToModel(this UserDtoBase userDtoBase)
+        {
+            return new UserModel(userDtoBase.UserId, userDtoBase.FirstName, userDtoBase.LastName);
+        }
+
+        public static UserDto ToDto(this UserModelBase userModelBase)
+        {
+            return new UserDto(userModelBase.UserId, userModelBase.FirstName, userModelBase.LastName);
+        }
+    }
+}
