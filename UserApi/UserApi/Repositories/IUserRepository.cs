890d2ea0c9f31c1c0a21eedc87319a699b78f79f
rahmat <rahmat.boakye-agyemang@microlise.com>
2021-12-27 21:58:14 +0000

    Adding UserRepository

---
+++ UserApi/UserApi/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using UserApi.Dtos;
+
+namespace UserApi.Repositories
+{
+    public interface IUserRepository
+    {
+        IEnumerable<UserDto> GetAllUsers();
+
+        UserDtoBase GetById(int id);
+
+        bool TryUpdateUser(UserDto user);
+
+        bool TryDeleteUser(int userId);
+
+        bool TryCreateUser(UserDto user);
+    }
+}
---
+++ UserApi/UserApi/Repositories/UserRepository.cs
@@ -0,0 +1,70 @@
+﻿using UserApi.Dtos;
+using UserApi.Helpers;
+
+namespace UserApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDatasource _datasource;
+
+        public UserRepository(IDatasource datasource)
+        {
+            _datasource = datasource;
+        }
+
+        public bool TryCreateUser(UserDto user)
+        {
+            UserDto existingUser;
+
+            if (_datasource.Users.TryGetValue(user.UserId, out existingUser))
+            {
+                return false;
+            }
+
+            _datasource.Users.Add(user.UserId, user);
+            return true;
+        }
+
+        public bool TryDeleteUser(int userId)
+        {
+            return _datasource.Users.Remove(userId);
+        }
+
+        public IEnumerable<UserDto> GetAllUsers()
+        {
+            var users = _datasource.Users;
+
+            return users.Values;
+        }
+
+        public UserDtoBase GetById(int id)
+        {
+            UserDto existingUser;
+
+            if (_datasource.Users.TryGetValue(id, out existingUser))
+            {
+                return existingUser;
+            }
+
+            return new NullUserDto();
+        }
+
+        public bool TryUpdateUser(UserDto user)
+        {
+            UserDto existingUser;
+
+            if (_datasource.Users.TryGetValue(user.UserId, out existingUser))
+            {
+                var removedSuccessfully = _datasource.Users.Remove(user.UserId);
+
+                if (removedSuccessfully)
+                {
+                    _datasource.Users.Add(user.UserId, user);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
