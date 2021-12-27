60fc543e94c545fa01ea9d2f4038ca8bf5aac522
rahmat <rahmat.boakye-agyemang@microlise.com>
2021-12-27 21:58:53 +0000

    Adding UserService

---
+++ UserApi/UserApi/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using UserApi.Models;
+
+namespace UserApi.Services
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetAllUsers();
+
+        UserModelBase GetUser(int userId);
+
+        bool TryUpdateUser(UserModel user);
+
+        bool TryDeleteUser(int userId);
+
+        bool TryCreateUser(UserModel user);
+    }
+}
---
+++ UserApi/UserApi/Services/UserService.cs
@@ -0,0 +1,59 @@
+﻿using UserApi.Dtos;
+using UserApi.Mappers;
+using UserApi.Models;
+using UserApi.Repositories;
+
+namespace UserApi.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public bool TryCreateUser(UserModel user)
+        {
+            var userDto = user.ToDto();
+
+            return _userRepository.TryCreateUser(userDto);
+        }
+
+        public bool TryDeleteUser(int userId)
+        {
+            return _userRepository.TryDeleteUser(userId);
+        }
+
+        public UserModelBase GetUser(int userId)
+        {
+            var userDtoBase = _userRepository.GetById(userId);
+
+            if (userDtoBase.UserId.Equals(0))
+            {
+                return new NullUserModel();
+            }
+
+            return userDtoBase.ToModel();
+        }
+
+        public bool TryUpdateUser(UserModel user)
+        {
+            return _userRepository.TryUpdateUser(user.ToDto());
+        }
+
+        public IEnumerable<UserModel> GetAllUsers()
+        {
+            var userDtos = _userRepository.GetAllUsers();
+
+            foreach(var userDto in userDtos)
+            {
+                var userModel = userDto.ToModel();
+                yield return userModel; 
+            }
+
+           yield break;
+        }
+    }
+}
