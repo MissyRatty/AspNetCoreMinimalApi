57dc035970fc99a5bb1912223496830cfd20537d
rahmat <rahmat.boakye-agyemang@microlise.com>
2021-12-27 21:59:02 +0000

    Adding IoC

---
+++ UserApi/UserApi/IoC/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UserApi.Helpers;
+using UserApi.Repositories;
+using UserApi.Services;
+
+namespace UserApi.IoC
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddRepositories(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IUserRepository, UserRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IUserService, UserService>();
+        }
+
+        public static void AddDataSources(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IDatasource, InMemoryDatasource>();
+        }
+    }
+}
